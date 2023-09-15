386e6c16b4cdc6f6e7241ed54e34e745f1fa2131
mkowack <kowackag@gmail.com>
2023-09-15 21:19:52 +0300

    Eslint fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,5 @@
-import { Product } from "@/components/Product";
+import { Product } from "@/app/Product";
+
 interface ProductProps {
 	id: number;
 	title: string;
@@ -10,21 +11,14 @@ interface ProductProps {
 }
 
 const Products = ({ data }: { data: ProductProps[] }) => {
-	if (!data) {
-		return (
-			<div className="flex-grow px-8 py-8 text-2xl text-zinc-700">
-				<p>Loading....</p>
-			</div>
-		);
-	}
-
 	return (
 		<div className=" mx-2 my-2 flex items-center justify-between">
 			<ul
 				className="grid grow grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-3"
 				data-testid="products-list"
 			>
-				{data.length &&
+				{data &&
+					data.length &&
 					data.map((item) => (
 						<li key={item.id}>
 							<Product data={item} />
